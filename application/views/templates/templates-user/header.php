e55f8cddac7781e22a79e6c51c79ed1201219def
syaifull09 <msyaiful093@gmail.com>
2024-05-07 18:52:13 +0700

    push Pertemuan 4 dan 5

--- application/views/templates/templates-user/header.php
+++ application/views/templates/templates-user/header.php
@@ -15,7 +15,7 @@ type="text/css">
  <nav class="navbar navbar-expand-lg navbar-light bg-light">
  <div class="container">
  <a class="navbar-brand" href="<?=base_url();?>">Pustaka</a>
- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" arialabel="Toggle navigation">
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
  <span class="navbar-toggler-icon"></span>
  </button>
  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
@@ -23,10 +23,13 @@ type="text/css">
  <a class="nav-item nav-link active" href="<?=base_url();?>">Beranda <span class="sr-only">(current)</span></a>
  <?php
 if (!empty($this->session->userdata('email'))) {?>
- <a class="nav-item nav-link" href="#">Booking Buku</a>
+ <!--<a class="nav-item nav-link" href="#">Booking Buku</a>-->
+ <a class="nav-item nav-link" href="<?=base_url('booking');?>">Booking <b><?=$this->ModelBooking->getDataWhere('temp', ['email_user' => $this->session->userdata('email')])->num_rows();?></b> Buku</a>
+ <a class="nav-item nav-link" onclick="information('Waktu Pengambilan Buku 2x24 jam dari Booking!!!')" href="<?php echo base_url() . 'booking/exportToPdf/' . $this->session->userdata('id_user'); ?>">Cetak Bukti Booking</a>
  <a class="nav-item nav-link" href="<?=base_url('member/myprofil');?>">Profil Saya</a>
- <a class="nav-item nav-link" href="<?=base_url('member/logout');?>"><i class="fas fw
-fa-login"></i> Log out</a>
+ <a class="nav-item nav-link" href="<?=base_url('member/logout');?>"><i class="fas fwfa-login"></i> Log out</a>
+
+
  <?php } else {?>
  <a class="nav-item nav-link" data-toggle="modal" data-target="#daftarModal"href="#"><i class="fas fw fa-login"></i> Daftar</a>
  <a class="nav-item nav-link" data-toggle="modal" data-target="#loginModal"href="#"><i class="fas fw fa-login"></i> Log in</a>
